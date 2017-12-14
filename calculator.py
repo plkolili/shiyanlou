f9ec8f77ecff73b518d8be5715b217a8fe3b6b63
1021188397 <1021188397@qq.com>
2017-12-14 19:11:30 +0800

    new

--- calculator.py
+++ calculator.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python3
 import sys
-#print(len(sys.argv))
-try:
-    a=int(sys.argv[1])
-except ValueError:
-    print("enter interger")
+
+for arg in sys.argv[1:]:
+    spl=arg.split(':')
+    a=int(spl[1])
+    number=int(spl[0])
+   
+
+#    print("enter interger")
 #if __name__=='__main__':
-#    for arg in sys.argv:
-#        print(arg)
-b=a-a*0.165-3500
+    
+    b=a-a*0.165-3500
 
-if b<=0:
-    c=0
-elif 0<b<=1500:
-    c=b*0.03-0
-elif 1500<b<=4500:
-    c=b*0.1-105
-elif 4500<b<=9000:
-    c=b*0.2-555
-elif 9000<b<=35000:
-    c=b*0.25-1005
-elif 35000<b<=55000:
-    c=b*0.30-2755
-elif 55000<b<=80000:
-    c=b*0.35-5505
-else:
-    c=b*0.45-13505
-
-print(format(c,".2f"))
+    if b<=0:
+        c=0
+    elif 0<b<=1500:
+        c=b*0.03-0
+    elif 1500<b<=4500:
+        c=b*0.1-105
+    elif 4500<b<=9000:
+        c=b*0.2-555
+    elif 9000<b<=35000:
+        c=b*0.25-1005
+    elif 35000<b<=55000:
+        c=b*0.30-2755
+    elif 55000<b<=80000:
+        c=b*0.35-5505
+    else:
+        c=b*0.45-13505
+#    print(number,':')
+    print(number,':',format(a-a*0.165-c,".2f"))
