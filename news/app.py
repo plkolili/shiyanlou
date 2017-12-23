28976c8a2dc8ba61d4cfa33b23399d6097a6fbfd
plkolili <32972400+plkolili@users.noreply.github.com>
2017-12-23 19:35:20 +0800

    Update app.py

--- news/app.py
+++ news/app.py
@@ -1,14 +1,14 @@
 from flask import Flask,render_template
-import os
+import os,json
 app=Flask(__name__)
-app.config('TEMPLATE_AUTO_RELOAD']=True
+app.config['TEMPLATE_AUTO_RELOAD']=True
 @app.route('/')
 def index():
     path='/home/shiyanlou/files'
     files=os.listdir(path=path)
     files1=os.path.join(path,files[0])
     with open( files1,'r') as f:
-        shiyanlou=json.loads(f())
+        shiyanlou=json.loads(f.read())
     return render_template('index.html',shiyanlou=shiyanlou) 
 
 
