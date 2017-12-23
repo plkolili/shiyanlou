1a9357e59df951de2c0996d84fe52f8b533f48de
plkolili <32972400+plkolili@users.noreply.github.com>
2017-12-23 19:19:44 +0800

    Update app.py

--- news/app.py
+++ news/app.py
@@ -4,14 +4,17 @@ app=Flask(__name__)
 app.config('TEMPLATE_AUTO_RELOAD']=True
 @app.route('/')
 def index():
-    os.
+    path='/home/shiyanlou/files'
+    files=os.listdir(path=path)
+    files1=os.path.join(path,files[0])
+    with open( files1,'r') as f:
+        shiyanlou=json.loads(f())
+    return render_template('index.html',shiyanlou=shiyanlou) 
 
-    return render_template('index.html',) 
 
-
-@app.route('/files/<filename>')
-def file(filename):
-    os.
-
-    return render_template()
+#@app.route('/files/<filename>')
+#def file(filename):
+#    os.
+#
+#    return render_template()
 
